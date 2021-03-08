b4136fd610ca0c8785ef18a3b0d7047bdbe5a707
devcjlozano <carlosjlp@gmail.com>
2021-03-08 17:03:14 +0100

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <h1> {{ count }} </h1>
+  <HelloWorld
+    @count-add="payload => count += payload.number"
+    @count-remove="count --"
+    msg="Hello Vue 3.0 + Vite"/>
+</template>
+
+<script>
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    const count = ref(0)
+    return { count }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1> {{ msg }}</h1>
+  <button @click="add"> Add </button>
+  <button @click="remove"> Remove </button>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  // Documentation
+  // emits: ['count-add', 'count-remove']
+  // Validation
+  emits: {
+    'count-remove': null,
+    'count-add': payload => payload && payload.number && typeof payload.number === "number" ? true : false 
+  },
+  setup(props, { emit }) {
+    function add() {
+      emit('count-add', { number: "10" })
+    }
+    function remove() {
+      emit('count-remove')
+    }
+
+    return { add, remove }
+  }
+  
+}
+</script>
